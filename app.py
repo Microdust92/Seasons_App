a5b00b67addf9056cd1ca28d221135607d5c262d
Microdust92 <167494211+Microdust92@users.noreply.github.com>
2025-09-27 03:24:21 -0500

    Ice cream subject 1st draft

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
+import random
 
 app = Flask(__name__)
 
@@ -10,3 +11,27 @@ def home():
     current_year = now.year
     current_month = now.strftime("%B")  # "September"
     return render_template("index.html", year=current_year, month=current_month)
+
+
+
+@app.route("/icecream")
+
+def icecream():
+    cone = ["waffle cone", "wafer cone"]
+    flavors = ["Vanilla", "Strawberry", "Chocolate", "Cookies Dough", "Mint"]
+    toppings = ["Sprinkles", "Cherry", "Chocolate syrup", "Strawberry syrup", "Caramel", "Chocolate Chip Cookies", "Whipped Cream"," "]
+
+    flavor_choice = random.choice(flavors)
+    topping_choice = random.choice(toppings)
+    topping_choice2 = random.choice(toppings)
+    topping_choice3 = random.choice(toppings)
+    cone_choice = random.choice(cone)
+
+    icecream_mix = f"{flavor_choice} Ice cream with {topping_choice} and {topping_choice2} and {topping_choice3} on a {cone_choice}"
+    return render_template("subject.html", icecream_mix=icecream_mix)
+
+
+@app.route("/icecream/result")
+
+def icecream_results():
+    return "Final Ice Cream results"
